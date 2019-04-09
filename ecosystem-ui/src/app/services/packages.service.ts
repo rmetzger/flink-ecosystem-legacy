369bd68d3ad0712d6471b8cb0e5471118c053054
Daryl Roberts <daryl.roberts@gmail.com>
2019-04-09 11:53:48 +0200

    [front-end] trying to get routing working

--- ecosystem-ui/src/app/services/packages.service.ts
+++ ecosystem-ui/src/app/services/packages.service.ts
@@ -1,29 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
-
-interface Votes {
-  username: string;
-  repo: string;
-}
-
-export interface Package {
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  website: string;
-  repository: string;
-  license: string;
-  upvotes: Array<Votes>;
-  downvotes: Array<Votes>;
-  tags: Array<string>;
-  added: string;
-}
-
-interface PackageList {
-  items: Array<Package>;
-}
+import { PackageList } from "../interfaces/package";
 
 @Injectable({ providedIn: "root" })
 export class PackagesService {
